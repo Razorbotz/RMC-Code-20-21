132c1411158c114d5f11c36ec17669eda5ee464b
calvinfranz <cmfranz@uark.edu>
2021-03-13 12:52:36 -0600

    Add initial positional tracking script

---
+++ track_pos.py
@@ -0,0 +1,38 @@
+import pyzed.sl as sl
+import time
+
+# Create a Camera object
+zed = sl.Camera()
+
+# Set configuration parameters
+init_params = sl.InitParameters()
+init_params.camera_resolution = sl.RESOLUTION.HD720 # Use HD720 video mode (default fps: 60)
+init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP # Use a right-handed Y-up coordinate system
+init_params.coordinate_units = sl.UNIT.METER # Set units in meters
+
+# Open the camera
+err = zed.open(init_params)
+if err != sl.ERROR_CODE.SUCCESS:
+    exit(1)
+
+# Enable positional tracking with default parameters
+tracking_parameters = sl.PositionalTrackingParameters()
+err = zed.enable_positional_tracking(tracking_parameters)
+
+runtime_parameters = sl.RuntimeParameters()
+
+for frame in range(6):
+    zed_pose = sl.Pose()
+    if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+        # Get the pose of the camera relative to the world frame
+        zed.get_position(zed_pose, sl.REFERENCE_FRAME.FRAME_WORLD)
+        # Display translation and timestamp
+        py_translation = sl.Translation()
+        tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
+        ty = round(zed_pose.get_translation(py_translation).get()[1], 3)
+        tz = round(zed_pose.get_translation(py_translation).get()[2], 3)
+        print("Translation: tx: {0}, ty:  {1}, tz:  {2}, timestamp: {3}\n".format(tx, ty, tz, zed_pose.timestamp))
+    time.sleep(5)
+
+# Close the camera
+zed.close()
